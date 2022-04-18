79db96bab616b93dfa4baf14241b26ccc6698392
Radwa <radwa@efly.com>
2022-04-18 15:59:14 +0200

    Adding utils files

---
+++ app/src/main/java/com/radwaelsahn/currencyapp/utils/JsonReader.kt
@@ -0,0 +1,12 @@
+package com.radwaelsahn.currencyapp.utils
+
+import android.content.Context
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.radwaelsahn.currencyapp.data.models.Currency
+import java.io.BufferedReader
+import java.io.InputStream
+
+
+
+
---
+++ app/src/main/java/com/radwaelsahn/currencyapp/utils/Keys.kt
@@ -0,0 +1,7 @@
+package com.radwaelsahn.currencyapp.utils
+
+object Keys {
+
+    const val KEY_CHARACTER_ID = "KEY_CHARACTER_ID"
+    const val KEY_CHARACTER_OBJ = "KEY_CHARACTER_OBJ"
+}
